5f25fb26971ef08b3e4584a9350d5608fc3141ed
Samir <samir@nanosurfacebio.com>
2020-07-16 00:57:34 +0000

    Added method within PlateRecordings to combine data into 1 csv

--- src/curibio/sdk/files.py
+++ src/curibio/sdk/files.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 """Classes and functinos for finding and managing files."""
+import inspect
+import os
+from typing import List
+
 import h5py
 from nptyping import NDArray
 import numpy as np
+
+PATH_OF_CURRENT_FILE = os.path.dirname((inspect.stack()[0][1]))
 
 
 class WellFile:
@@ -26,7 +32,7 @@ class WellFile:
     def get_well_index(self) -> int:
         return int(self._h5_file.attrs["Well Index (zero-based)"])
 
-    def get_numpy_array(self) -> NDArray[2, int]:
+    def get_numpy_array(self) -> NDArray[2, float]:
         """Return the data (tissue sensor vs time)."""
         time_step = 8 * 1.2e-3  # tissue sample rate is just over 100Hz
         tissue_data = self._h5_file["tissue_sensor_readings"]
@@ -41,7 +47,7 @@ class WellFile:
 
         return data
 
-    def get_voltage_array(self) -> NDArray[2, int]:
+    def get_voltage_array(self) -> NDArray[2, float]:
         """Return the voltage vs time data."""
         time_step = 8 * 1.2e-3  # tissue sample rate is just over 100Hz
         vref = 3.3  # ADC reference voltage
@@ -62,3 +68,33 @@ class WellFile:
             voltage_data[1, i] = voltages[i]
 
         return voltage_data
+
+
+class PlateRecording:
+    """Wrapper around 24 WellFiles fpr a single plate of data.
+
+    Args:
+        file_paths: A list of all the file paths for each h5 file to open.
+
+    Attributes:
+        _files_ : WellFiles of all the file paths provided.
+    """
+
+    def __init__(self, file_paths: List[str]) -> None:
+        self._files_: List[str] = file_paths
+
+    def get_wellfile_names(self) -> List[str]:
+        well_files = []
+        for well in self._files_:
+            well_files.append(WellFile(well).get_well_name())
+        return well_files
+
+    def get_combined_csv(self) -> None:
+        data = np.zeros((len(self._files_) + 1, 12315))
+        for i, well in enumerate(self._files_):
+            well_data = WellFile(well).get_numpy_array()
+            data[0, :] = well_data[0, :]
+            data[i + 1, :] = well_data[1, :]
+
+        my_local_path_data = os.path.join(PATH_OF_CURRENT_FILE, "PlateRecording.csv")
+        np.savetxt(my_local_path_data, data, delimiter=",", fmt="%d")
--- tests/test_files.py
+++ tests/test_files.py
@@ -2,6 +2,7 @@
 import inspect
 import os
 
+from curibio.sdk import PlateRecording
 from curibio.sdk import WellFile
 import numpy as np
 
@@ -36,3 +37,31 @@ def test_WellFile__opens_and_get_voltage_array():
     )
 
     assert np.size(wf.get_voltage_array()) == 25986
+
+
+def test_PlateRecording__opens_and_get_wellfiles():
+    wf1 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B1.h5"
+    )
+    wf2 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B2.h5"
+    )
+    wf3 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B3.h5"
+    )
+    wf4 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B4.h5"
+    )
+    wf5 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B5.h5"
+    )
+    wf6 = os.path.join(
+        PATH_OF_CURRENT_FILE, "h5_New", "my_barcode__2020_05_24_203716__B6.h5"
+    )
+
+    files = PlateRecording([wf1, wf2, wf3, wf4, wf5, wf6])
+
+    # test csv writer
+    files.get_combined_csv()
+
+    assert np.size(files.get_wellfile_names()) == 6

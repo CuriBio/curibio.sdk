d6c5c77ce8a71f7fa34577c9e809e278a409a17b
tanner <tanner@nanosurfacebio.com>
2020-07-29 17:37:19 +0000

    bumping version no. for CodeBuild test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 
 setup(
     name="curibio.sdk",
-    version="0.1.1",
+    version="0.1.2",
     description="CREATE A DESCRIPTION",
     url="https://github.com/CuriBio/curibio.sdk",
     author="Curi Bio",
